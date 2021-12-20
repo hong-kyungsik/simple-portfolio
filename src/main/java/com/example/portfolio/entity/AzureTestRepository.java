42a3895b46de0c8fc99431fc814110be0e07c732
hong-kyungsik <h.ks.dpr@gmail.com>
2021-12-21 00:04:12 +0900

    Remove. remove files related to Azure Test

--- src/main/java/com/example/portfolio/entity/AzureTestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.portfolio.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AzureTestRepository extends JpaRepository<AzureTest, Long> {
-}
